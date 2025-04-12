defe2dc6dc88bfc129b9972c1626dac4ddc9a843
Rodney <rodney.amani@student.moringaschool.com>
2025-04-12 14:00:30 +0300

    completed

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import Keypad from './components/Keypad';
+import EyesOnMe from './components/EyesOnMe';
+
+function App() {
+  return (
+    <div>
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
